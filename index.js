3593c9cc17864463bc4ea3e65c2ce4820c2db522
deepanshusac-maker <deepanshusac@gmail.com>
2026-01-06 11:25:39 +0530

    Add mobile navigation toggle button
    Introduced a hamburger menu icon in the navigation bar for mobile view. Added the myFunction JavaScript to toggle the display of navigation links, improving mobile usability.

--- index.js
+++ index.js
@@ -385,3 +385,11 @@ readPaperLinks.forEach(link => {
         });
     }
 });
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
